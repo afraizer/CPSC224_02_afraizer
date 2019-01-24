33e198f96f497bfa0d7cafa42f359f96cc921f89
afraizer <46797580+afraizer@users.noreply.github.com>
2019-01-24 13:03:52 -0800

    Add files via upload

---
+++ HW1.java
@@ -0,0 +1,25 @@
+public class HW1
+{
+	//Functions headers
+	int rollDice();
+	int maxOfAKindFound(int []);
+	int maxStraightFound(int []);
+	bool fullHouseFound(int []);
+	int totalAllDice(int []);
+	void sortArray(int [], int);
+	
+	public static void main(String [] args)
+	{
+		final int DICE_IN_PLAY = 5;
+		int hand[DICE_IN_PLAY];
+		//srand (time(0));
+		char playAgain = 'y';
+		
+		while(playAgain == 'y')
+		{
+			
+		}
+		
+		
+	}
+}
